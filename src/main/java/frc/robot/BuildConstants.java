5183dadbdfd16958a34c12fd4565af1db9241d25
Vishnu Bharath <80052621+TheAmazingWizard@users.noreply.github.com>
2025-01-14 13:33:47 -0500

    photonlib vendordep install + wpilib -> 2025.2.1

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,15 +3,15 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Real Swerve 25";
+  public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = -1;
-  public static final String GIT_SHA = "UNKNOWN";
-  public static final String GIT_DATE = "UNKNOWN";
-  public static final String GIT_BRANCH = "UNKNOWN";
-  public static final String BUILD_DATE = "2025-01-13 17:53:24 EST";
-  public static final long BUILD_UNIX_TIME = 1736808804838L;
-  public static final int DIRTY = 129;
+  public static final int GIT_REVISION = 14;
+  public static final String GIT_SHA = "126412e57b1233b6e1deb470595b30b44c7f6b39";
+  public static final String GIT_DATE = "2025-01-13 20:52:42 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-01-14 13:28:33 EST";
+  public static final long BUILD_UNIX_TIME = 1736879313459L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
