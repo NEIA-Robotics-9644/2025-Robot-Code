a5d2626c6df106ce46869dfed9431e7155dbe163
SamJackson24816 <samj24816@gmail.com>
2025-01-13 20:42:07 -0500

    Old stuff

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Real Swerve 25";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = -1;
+  public static final String GIT_SHA = "UNKNOWN";
+  public static final String GIT_DATE = "UNKNOWN";
+  public static final String GIT_BRANCH = "UNKNOWN";
+  public static final String BUILD_DATE = "2025-01-13 17:53:24 EST";
+  public static final long BUILD_UNIX_TIME = 1736808804838L;
+  public static final int DIRTY = 129;
+
+  private BuildConstants() {}
+}
