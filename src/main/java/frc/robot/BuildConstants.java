3739009c8f175939c0ba8f9c392ef5c9d888874a
Vishnu Bharath <80052621+TheAmazingWizard@users.noreply.github.com>
2025-01-16 16:00:37 -0500

    single cam for now

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 18;
-  public static final String GIT_SHA = "8377521f41fd4f0815cbe93074b2c6ce9e477a1e";
-  public static final String GIT_DATE = "2025-01-14 13:33:54 EST";
+  public static final int GIT_REVISION = 19;
+  public static final String GIT_SHA = "2814aa224ac92517c82ae47a91358486816acdbb";
+  public static final String GIT_DATE = "2025-01-15 15:38:30 EST";
   public static final String GIT_BRANCH = "vision";
-  public static final String BUILD_DATE = "2025-01-15 12:42:41 EST";
-  public static final long BUILD_UNIX_TIME = 1736962961785L;
+  public static final String BUILD_DATE = "2025-01-15 22:10:56 EST";
+  public static final long BUILD_UNIX_TIME = 1736997056874L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
