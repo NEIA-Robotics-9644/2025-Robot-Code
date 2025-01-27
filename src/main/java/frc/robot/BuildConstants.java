bf1148938a5cb42b12445bd0741bda069af855e4
SamJackson24816 <samj24816@gmail.com>
2025-01-27 16:39:46 -0500

    Added End Effector Wheels Subsystem Design

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Real Swerve 25";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 35;
-  public static final String GIT_SHA = "2345ef39240380be8f389f65ff4ad93384292e82";
-  public static final String GIT_DATE = "2025-01-27 16:01:57 EST";
+  public static final int GIT_REVISION = 37;
+  public static final String GIT_SHA = "0e43d5fad6283884208f34a84deb1547ffa71ec5";
+  public static final String GIT_DATE = "2025-01-27 16:26:05 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-27 16:03:01 EST";
-  public static final long BUILD_UNIX_TIME = 1738011781665L;
+  public static final String BUILD_DATE = "2025-01-27 16:37:34 EST";
+  public static final long BUILD_UNIX_TIME = 1738013854853L;
   public static final int DIRTY = 0;
 
   private BuildConstants() {}
---
+++ src/main/java/frc/robot/subsystems/end_effector_wheels/end_effector_wheels_design.md
@@ -0,0 +1,10 @@
+
+
+# End Effector Wheels
+
+The end effector is the mechanism mounted on the pivot at the end of the elevator.
+The end effector has wheels which spin in opposite directions and push the coral through.
+
+## Wheel Motor
+- The wheel motor is a NEO directly spinning a chain.
+- The wheel motor is run with velocity control (in-motor if possible).
