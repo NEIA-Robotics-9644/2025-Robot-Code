1c97113ce998b4d5f8fbd9415a7da8d04b3a510a
SamJackson24816 <samj24816@gmail.com>
2025-01-27 17:23:10 -0500

    Updated path coordinates and rotation values for path planning

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Real Swerve 25";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 37;
-  public static final String GIT_SHA = "0e43d5fad6283884208f34a84deb1547ffa71ec5";
-  public static final String GIT_DATE = "2025-01-27 16:26:05 EST";
+  public static final int GIT_REVISION = 38;
+  public static final String GIT_SHA = "bf1148938a5cb42b12445bd0741bda069af855e4";
+  public static final String GIT_DATE = "2025-01-27 16:39:46 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-27 16:37:34 EST";
-  public static final long BUILD_UNIX_TIME = 1738013854853L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2025-01-27 17:14:54 EST";
+  public static final long BUILD_UNIX_TIME = 1738016094842L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
