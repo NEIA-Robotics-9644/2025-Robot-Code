e95447d045653b628f1c2960c694c58d12743129
Sam Jackson <samj24816@gmail.com>
2025-02-11 16:17:41 -0500

    Started making flywheel io implementations

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 49;
-  public static final String GIT_SHA = "257f592426f925c5d6e2f7d41b1bf7b15e54d684";
-  public static final String GIT_DATE = "2025-02-06 22:22:34 EST";
+  public static final int GIT_REVISION = 51;
+  public static final String GIT_SHA = "d3c468653d4ccbe5c231d0110a2a4eedc40fb669";
+  public static final String GIT_DATE = "2025-02-07 17:20:54 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-02-06 22:28:14 EST";
-  public static final long BUILD_UNIX_TIME = 1738898894629L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2025-02-10 17:25:32 EST";
+  public static final long BUILD_UNIX_TIME = 1739226332915L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
