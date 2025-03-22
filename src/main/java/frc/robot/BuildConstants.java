b7f51e7339bdafe07c179ad9ff762bf621888310
NEIA Robotics <148218798+neiarobotics@users.noreply.github.com>
2025-03-21 21:43:25 -0400

    auto path changes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 121;
-  public static final String GIT_SHA = "8b7940873a91ea661ddb9f9cfc008c17cbe87d90";
-  public static final String GIT_DATE = "2025-03-21 16:19:10 EDT";
+  public static final int GIT_REVISION = 122;
+  public static final String GIT_SHA = "f3fceb12da0fc9c7223a7e4726ed4bc6dd4d7088";
+  public static final String GIT_DATE = "2025-03-21 19:32:47 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-21 18:24:04 EDT";
-  public static final long BUILD_UNIX_TIME = 1742595844586L;
+  public static final String BUILD_DATE = "2025-03-21 20:35:18 EDT";
+  public static final long BUILD_UNIX_TIME = 1742603718233L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
