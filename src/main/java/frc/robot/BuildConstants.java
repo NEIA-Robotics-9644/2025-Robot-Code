d0079b647f3b25b515c5afa879450893ae0ebf6f
SamJackson24816 <samj24816@gmail.com>
2025-03-21 22:20:35 -0400

    Slowed max speed of autos

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 122;
-  public static final String GIT_SHA = "f3fceb12da0fc9c7223a7e4726ed4bc6dd4d7088";
-  public static final String GIT_DATE = "2025-03-21 19:32:47 EDT";
+  public static final int GIT_REVISION = 123;
+  public static final String GIT_SHA = "b7f51e7339bdafe07c179ad9ff762bf621888310";
+  public static final String GIT_DATE = "2025-03-21 21:43:25 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-21 20:35:18 EDT";
-  public static final long BUILD_UNIX_TIME = 1742603718233L;
+  public static final String BUILD_DATE = "2025-03-21 22:09:10 EDT";
+  public static final long BUILD_UNIX_TIME = 1742609350183L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
